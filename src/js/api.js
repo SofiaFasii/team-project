3daabf27c9c33598cd933acca96eea3cbd906f84
SofiaFasii <sofiafasii97@gmail.com>
2026-05-12 21:59:29 +0300

    о

--- src/js/api.js
+++ src/js/api.js
@@ -1,11 +1,17 @@
 const BASE_URL = "https://app.ticketmaster.com/discovery/v2/events.json"
 const API_KEY = "tG7aQ3GCK35UI1b0FVQGBmQ2kSZJ2t4e"
 
-export async function fetchEvents({country, keyword, page}) {
+let currentPages = 0
+let totalPages = 0
+
+export async function fetchEvents({country, keyword, page = 0}) {
   const URL = `${BASE_URL}?apikey=${API_KEY}&keyword=${keyword}&countryCode=${country}&page=${page}`
 
   const res = await fetch(URL);
   const data = await res.json();
-
+  totalPages = data.page.totalPages
+  currentPages = data.page.number
+  //викликати рендер подіїб, потім видалити return
+  //викликати пагінацію
   return data;
 }

50c9dbd010e84e863a6bbd2f2596fae288cc6f45
SofiaFasii <sofiafasii97@gmail.com>
2026-05-13 10:40:31 +0300

    модалка

--- src/js/modal.js
+++ src/js/modal.js
@@ -5,16 +5,23 @@ const overlay = document.querySelector(".modal-container");
 export function renderModal(event) {
   overlay.innerHTML = `
     <div class="modal">
-      <button id="closeBtn">×</button>
-      <h2 class="h2-info">Info</h2>
-      <p class="p-info">${event.info}</p>
-      <h2 class="h2-when">When</h2>
-      <p class="p-data">${event.dates.start.localDate}</p>
-      <p class="p-time">${event.dates.start.localTime}</p>
-      <h2 class="h2-where">Where</h2>
-      <p class="p-where">${event.locale}</p>
-      <h2 class="h2-who">Who</h2>
-      <p class="p-who">${event.name}</p>
+      <div class="modal-wrapper">
+      <button id="closeBtn">&times;</button>
+        <img class="modal-img" src="${event.images[0].url}" alt="${event.name}">
+        <div class="modal-text">
+          <h2 class="h2-info">Info</h2>
+          <p class="p-info">${event.info}</p>
+          <h2 class="h2-when">When</h2>
+          <p class="p-data">${event.dates.start.localDate}</p>
+          <p class="p-time">${event.dates.start.localTime}</p>
+          <h2 class="h2-where">Where</h2>
+          <p class="p-where">${event._embedded.venues[0].city.name}</p>
+          <p class="modal-venues">${event._embedded.venues[0].name}</p>
+          <h2 class="h2-who">Who</h2>
+          <p class="p-who">${event.name}</p>
+        </div>
+      </div>
+      <button class="modal-btn">MORE FROM THIS AUTHOR</button>
     </div>
   `;
 
